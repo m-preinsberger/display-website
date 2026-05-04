ba9d448b8b5794364da361436e87597606e152f6
m-preinsberger <20212015@atn.ac.at>
2026-05-04 17:44:27 +0200

    Remove vocab tab

--- alethia-web/src/App.vue
+++ alethia-web/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav class="site-tabs" aria-label="Main navigation">
     <RouterLink to="/">Portfolio</RouterLink>
-    <RouterLink to="/vocab">Vocabulary</RouterLink>
     <RouterLink to="/deutschmatura">Deutschmatura</RouterLink>
   </nav>
   <RouterView />
--- alethia-web/src/router.ts
+++ alethia-web/src/router.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PortfolioView from './views/PortfolioView.vue'
-import VocabView from './views/VocabView.vue'
 import DeutschMaturaView from './views/DeutschMaturaView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'portfolio', component: PortfolioView },
-    { path: '/vocab', name: 'vocab', component: VocabView },
     { path: '/deutschmatura', name: 'deutschmatura', component: DeutschMaturaView },
   ],
 })
