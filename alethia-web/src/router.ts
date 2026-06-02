67bd28330e75348aa1b94c8f43fa010a960a3640
m-preinsberger <20212015@atn.ac.at>
2026-06-02 12:21:43 +0200

    Remove Deutschmatura tab

--- alethia-web/src/router.ts
+++ alethia-web/src/router.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PortfolioView from './views/PortfolioView.vue'
-import DeutschMaturaView from './views/DeutschMaturaView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'portfolio', component: PortfolioView },
-    { path: '/deutschmatura', name: 'deutschmatura', component: DeutschMaturaView },
   ],
 })
 
